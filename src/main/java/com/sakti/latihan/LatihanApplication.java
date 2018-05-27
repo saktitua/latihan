1c0bbef7dc786a861c362328a268fd2b5ff02ed2
sakti <saktitua14@gmail.com>
2018-05-27 12:34:12 +0700

    first commit

---
+++ src/main/java/com/sakti/latihan/LatihanApplication.java
@@ -0,0 +1,12 @@
+package com.sakti.latihan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sakti/latihan/LatihanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sakti.latihan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LatihanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
